5835379bd53b57478e9230688bb2f959a04424d8
Biren soni <purvisoni09@gmail.com>
2020-11-19 12:57:06 -0600

    first commit

---
+++ ExpenseSystem.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace ExpenseManager
+{
+    public class ExpenseSystem
+    {
+        public void AddExpense()
+        {
+            Console.WriteLine("Adding an expense...");
+        }
+        public void ViewExpense()
+        {
+            Console.WriteLine("Viewing the expense...");
+        }
+        public void UpdateExpense()
+        {
+            Console.WriteLine("Updating the expense...");
+        }
+        public void DeleteExpense()
+        {
+            Console.WriteLine("Deleting the expense...");
+        }
+
+        public void visitedLocation()
+        {
+            Console.WriteLine("User visited location..");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ExpenseManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var theExpenseSystem=new ExpenseSystem();
+            Console.WriteLine("Welcome to Expense Manager Application!");
+
+            while(true)
+            {
+                Console.WriteLine("Please enter a command:c-to add expense; r-to view expense; u-to update expense; d-to delete expense; l-visited location; q-quit ");
+
+                string userInput=Console.ReadLine();
+
+                if(userInput=="c")
+                {
+                  theExpenseSystem.AddExpense();
+                }
+
+                if(userInput=="r")
+                {
+                  theExpenseSystem.ViewExpense();
+                }
+
+                if(userInput=="u")
+                {
+                  theExpenseSystem.UpdateExpense();;
+                }
+                if(userInput=="d")
+                {
+                  theExpenseSystem.DeleteExpense();
+                }
+                if(userInput=="l")
+                {
+                    theExpenseSystem.visitedLocation();
+                }
+
+                if(userInput=="q")
+                {
+                    //to quit
+                    break;
+                }
+            }
+        }
+    }
+}
